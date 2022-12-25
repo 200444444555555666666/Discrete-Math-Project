5d4e0c2f5c30bcaf0c8b688d48abbee8740054b1
200444444555555666666 <119174021+200444444555555666666@users.noreply.github.com>
2022-12-25 23:24:12 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace perfect_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter first number");
+            int firstnumber = int.Parse(Console.ReadLine());
+            Console.WriteLine("please enter last number");
+            int lastnumber = int.Parse(Console.ReadLine());
+
+            for (int n=firstnumber ;n<=lastnumber;n++)
+            {
+                int sum = 0;
+                for (int j = 1; j <= (n / 2); j++)
+                { 
+                   
+                if (n % j == 0)
+                    {
+                        sum = sum + j;
+                    }
+                
+                }
+                if (sum == n)
+                    Console.WriteLine(n);
+                   
+                
+            }
+        }
+    }
+}
